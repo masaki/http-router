b79f20be44807d3f78a6aab031e681506453307c
Dann <techmemo@gmail.com>
2008-12-13 09:26:19 +0000

    oops. /{format} to .{format}
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-Router/trunk@26630 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/Router/Builder/Base.pm
+++ lib/HTTP/Router/Builder/Base.pm
@@ -8,7 +8,7 @@ sub build_route_with_format {
     my $requirements = $args->{requirements} || {};
 
     return HTTP::Router::Route->new(
-        path         => $path . "/{format}",
+        path         => $path . ".{format}",
         params       => $args,
         conditions   => $conditions,
         requirements => $requirements,
