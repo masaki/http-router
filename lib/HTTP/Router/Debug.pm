c854601e3001086aff842e6c5ffa06b014a54ba3
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-04-08 00:04:05 +0900

    renamed debug table methods

--- lib/HTTP/Router/Debug.pm
+++ lib/HTTP/Router/Debug.pm
@@ -2,46 +2,50 @@ package HTTP::Router::Debug;
 
 use strict;
 use warnings;
-use HTTP::Router;
 use Text::SimpleTable;
 
-our @EXPORT = qw(show_table);
+our @EXPORT = qw(routing_table draw_routing_table);
 
-{
+sub import {
+    # TODO: into package
+    my $into = 'HTTP::Router';
+    eval "require $into; 1" or die $@;
+
     no strict 'refs';
     no warnings 'redefine';
     for my $keyword (@EXPORT) {
-        *{ 'HTTP::Router::' . $keyword } = \&{ $keyword };
+        *{ $into . '::' . $keyword } = \&{ __PACKAGE__ . '::' . $keyword };
     }
 }
 
-sub show_table {
-    my ( $self, ) = @_;
-    my $report = _make_table_report($self);
-    print $report . "\n";
+sub draw_routing_table {
+    my $table = shift->routing_table->draw;
+    print "$table\n";
 }
 
-sub _make_table_report {
-    my ( $self, ) = @_;
-    my $t = Text::SimpleTable->new(
-        [ 35, 'path' ],
-        [ 10, 'method' ],
-        [ 10, 'controller' ],
-        [ 10, 'action' ]
+sub routing_table {
+    my $self = shift;
+
+    my $table = Text::SimpleTable->new(
+        [qw(35 path)      ],
+        [qw(10 method)    ],
+        [qw(10 controller)],
+        [qw(10 action)    ],
     );
+
     for my $route ($self->routes) {
         my $method = $route->conditions->{method};
         $method = [ $method ] unless ref $method;
-        $t->row(
+
+        $table->row(
             $route->path,
-            join( ',', @$method ),
+            join(',', @$method),
             $route->params->{controller},
             $route->params->{action}
         );
     }
-    my $header = 'Routing Table:' . "\n";
-    my $table = $t->draw;
-    $header . $table . "\n";
+
+    return $table;
 }
 
 1;
@@ -50,9 +54,22 @@ sub _make_table_report {
 
 HTTP::Router::Debug
 
+=head1 SYNOPSIS
+
+    use HTTP::Router;
+    use HTTP::Router::Debug;
+
+    my $router = HTTP::Router->define(...);
+
+    print $router->routing_table->draw;
+    # or
+    $router->draw_routing_table;
+
 =head1 METHODS
 
-=head2 show_table
+=head2 routing_table
+
+=head2 draw_routing_table
 
 =head1 AUTHOR
 
