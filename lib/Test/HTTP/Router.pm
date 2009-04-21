b2a8061409a53510662254f57593283e7015d3d9
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-04-21 13:17:01 +0900

    added some tests, implemented params_ok(), params_not_ok() test methods

--- lib/Test/HTTP/Router.pm
+++ lib/Test/HTTP/Router.pm
@@ -4,44 +4,68 @@ use strict;
 use warnings;
 use Exporter 'import';
 use Test::Builder;
+use Test::Deep;
 use Test::MockObject;
 
 our @EXPORT = qw(
     path_ok path_not_ok
     match_ok match_not_ok
+    params_ok params_not_ok
 );
 
 our $Test = Test::Builder->new;
 
-sub request {
+sub to_request {
     my $args = ref $_[0] ? shift : { @_ };
-    my $req = Test::MockObject->new;
+    my $req  = Test::MockObject->new;
     $req->set_always($_ => $args->{$_}) for keys %$args;
     $req;
 }
 
 sub path_ok {
     my ($router, $path, $message) = @_;
-    my $req = request path => $path;
+    my $req = to_request(path => $path);
     $Test->ok($router->match($req) ? 1 : 0, $message || "matched $path");
 }
 
 sub path_not_ok {
     my ($router, $path, $message) = @_;
-    my $req = request path => $path;
+    my $req = to_request(path => $path);
     $Test->ok($router->match($req) ? 0 : 1, $message || "not matched $path");
 }
 
 sub match_ok {
-    my ($router, $path, $conditions, $message) = @_;
-    my $req = request %{ $conditions || {} }, path => $path;
-    $Test->ok($router->match($req) ? 1 : 0, $message || "matched $path with conditions");
+    my $router  = shift;
+    my $message = (@_ == 3 || @_ == 2 and not ref $_[-1]) ? pop : undef;
+    my $req     = ref $_[0] ? $_[0] : to_request(%{ $_[1] || {} }, path => $_[0]);
+    $Test->ok($router->match($req) ? 1 : 0, $message || "matched @{[$req->path]} with conditions");
 }
 
 sub match_not_ok {
-    my ($router, $path, $conditions, $message) = @_;
-    my $req = request %{ $conditions || {} }, path => $path;
-    $Test->ok($router->match($req) ? 0 : 1, $message || "not matched $path with conditions");
+    my $router  = shift;
+    my $message = (@_ == 3 || @_ == 2 and not ref $_[-1]) ? pop : undef;
+    my $req     = ref $_[0] ? $_[0] : to_request(%{ $_[1] || {} }, path => $_[0]);
+    $Test->ok($router->match($req) ? 0 : 1, $message || "not matched @{[$req->path]} with conditions");
+}
+
+sub params_ok {
+    my $router  = shift;
+    my $message = (@_ == 4 || @_ == 3 and not ref $_[-1]) ? pop : undef;
+    my $params  = $_[-1];
+    my $req     = ref $_[0] ? $_[0] : to_request(%{ $_[1] || {} }, path => $_[0]);
+
+    my $match = $router->match($req);
+    $Test->ok($match and eq_deeply($match->params, $params) ? 1 : 0, $message || "valid params at @{[$req->path]}");
+}
+
+sub params_not_ok {
+    my $router  = shift;
+    my $message = (@_ == 4 || @_ == 3 and not ref $_[-1]) ? pop : undef;
+    my $params  = $_[-1];
+    my $req     = ref $_[0] ? $_[0] : to_request(%{ $_[1] || {} }, path => $_[0]);
+
+    my $match = $router->match($req);
+    $Test->ok($match and eq_deeply($match->params, $params) ? 0 : 1, $message || "invalid params at @{[$req->path]}");
 }
 
 1;
